6024b5f2bf4348e231cd9a06d856d7063afae0cd
THERESA NYATICHI MONARI <140144885+Iamnyatichi@users.noreply.github.com>
2024-12-11 22:26:57 +0300

    Day 2 changes

---
+++ script.js
@@ -0,0 +1,20 @@
+// hiding the navigation bar on small screens
+var menu_hide = true;
+var menu_icons = document.getElementById("nav_bar_id");
+var btn = document.getElementById("menu_id");
+
+function show() {
+  if (menu_hide == true) {
+    menu_icons.classList.add("add_cls");
+    btn.innerHTML = "&times;";
+    btn.style.fontSize = "50px";
+
+    menu_hide = false;
+  } else {
+    menu_icons.classList.remove("add_cls");
+    btn.innerHTML = "&#9776;";
+    btn.style.fontSize = "30px";
+
+    menu_hide = true;
+  }
+}
