cc9cc9dd66570e5503d80af056e46acbbc0eeb2e
THERESA NYATICHI MONARI <140144885+Iamnyatichi@users.noreply.github.com>
2024-12-12 23:15:26 +0300

    Thursday update

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var menu_hide = true;
 var menu_icons = document.getElementById("nav_bar_id");
 var btn = document.getElementById("menu_id");
 
+//  Highlight active page
+// const activePage= window.location.pathname;
+// const navLinks = document.querySelectorAll(".header .nav_bar ul li a").
+// forEach(link => {
+  // if(link.href.includes(`${activePage}`)){
+  //  link.classList.add('active');
+  // }
+// });
+
 function show() {
   if (menu_hide == true) {
     menu_icons.classList.add("add_cls");
