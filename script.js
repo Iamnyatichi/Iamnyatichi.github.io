188204b4fe528e26d0a6ef2c7428ca65ff66c68e
THERESA NYATICHI MONARI <140144885+Iamnyatichi@users.noreply.github.com>
2025-01-13 14:19:28 +0300

    jan changes

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ var menu_hide = true;
 var menu_icons = document.getElementById("nav_bar_id");
 var btn = document.getElementById("menu_id");
 
-//  Highlight active page
-// const activePage= window.location.pathname;
-// const navLinks = document.querySelectorAll(".header .nav_bar ul li a").
-// forEach(link => {
-  // if(link.href.includes(`${activePage}`)){
-  //  link.classList.add('active');
-  // }
-// });
+// custom header and footer
+class SpecialHeader extends HTMLElement{
+  connectedCallback(){
+    
+  }
+}
 
 function show() {
   if (menu_hide == true) {
